13f8614c2f997e738082efcd334ff2d158aab326
jadsm <juanadelgadosm@gmail.com>
2026-02-11 18:45:12 +0000

    export addition and labels summary

--- sandbox-juan/export_test.py
+++ sandbox-juan/export_test.py
@@ -28,4 +28,29 @@ df = pdg.read_gbq(query, project_id="imperial-410612", dialect="standard")
 df.to_parquet("mhc_export.parquet", index=False)
 
 
+
+query = """select patient,`Group`,diagnosis_date,startTime startDate,variable,value  from `imperial-410612.MHC_PH.activity_us2`
+where 
+(patient in 
+(select patient
+from `imperial-410612.MHC_PH.activity_us2`
+where clean_status = 'clean_1'
+and device_rank = 'iPhone1'
+and cohort = 'UK' and variable in ('StepCount','FlightsClimbedPaceMax','StepCountPaceMax',
+                                'FlightsClimbedPaceMean','StepCountPaceMean'
+                                )
+                                and `Group` in ('Healthy')
+group by patient,`Group`,diagnosis_date
+having date_diff(max(startTime), min(startTime), DAY) >1000)
+or `Group` = 'PAH')
+and
+clean_status = 'clean_1'
+and device_rank = 'iPhone1'
+and cohort = 'UK' and variable in ('StepCount','FlightsClimbedPaceMax','StepCountPaceMax',
+                                'FlightsClimbedPaceMean','StepCountPaceMean'
+                               )
+"""
+df = pdg.read_gbq(query, project_id="imperial-410612", dialect="standard")
+df.to_parquet("mhc_phone_export.parquet", index=False)
+
 df = pd.read_parquet("mhc_export.parquet")
